bae694d9c242a1fbcf7c29c08039e2337cf11d29
Dhee-dishant <96163071+Dhee-dishant@users.noreply.github.com>
2024-11-23 14:29:37 +0530

    Update model_v2.py

--- model_v2.py
+++ model_v2.py
@@ -15,6 +15,47 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+class RotaryPositionalEmbeddings(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.base = config.base
+        self.d = config.n_embd // config.n_head
+        self.cos_cached = None
+        self.sin_cached = None
+
+
+    def _build_cache(self, x: torch.Tensor):
+        if self.cos_cached is not None and x.shape[2] <= self.cos_cached.shape[2]:
+           return
+        seq_len = x.shape[2]
+        
+        theta = 1. / (self.base ** (torch.arange(0, self.d, 2).float() / self.d)).to(x.device)
+        # 
+        seq_idx = torch.arange(seq_len, device=x.device).float().to(x.device)
+        # 
+        idx_theta = torch.einsum('n,d->nd', seq_idx, theta)
+        # 
+        idx_theta2 = torch.cat([idx_theta, idx_theta], dim=1)
+        self.cos_cached = idx_theta2.cos()[None, None, :, :]
+        self.sin_cached = idx_theta2.sin()[None, None, :, :]
+
+    # 
+    def _neg_half(self, x: torch.Tensor):
+        d_2 = self.d // 2
+        return torch.cat([-x[:, :, :, d_2:], x[:, :, :, :d_2]], dim=-1)
+    
+    def forward(self, x: torch.Tensor): # x (B, nh, T, hs)
+        #
+        self._build_cache(x)
+        # 
+        x_rope, x_pass = x[..., :self.d], x[..., self.d:]
+        # 
+        neg_half_x = self._neg_half(x_rope)
+        # 
+        x_rope = (x_rope * self.cos_cached[:x.shape[2]]) + (neg_half_x * self.sin_cached[:x.shape[2]])
+        #  
+        return torch.cat((x_rope, x_pass), dim=-1)
+
 class LayerNorm(nn.Module):
     """ LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False """
 
